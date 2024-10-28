ad1a56fbbec22af4fa2a51481e9017c5333c52ca
Yasantha Ravindu <99851400+Ravindu9EV@users.noreply.github.com>
2024-10-29 02:28:29 +0530

    create front-end

---
+++ src/app/page/patient-manage/patient-manage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-manage',
+  standalone: true,
+  imports: [],
+  templateUrl: './patient-manage.component.html',
+  styleUrl: './patient-manage.component.css'
+})
+export class PatientManageComponent {
+
+}
